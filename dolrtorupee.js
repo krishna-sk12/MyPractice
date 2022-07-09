2f37eebd15d79b37a8d3313bbe01c2003cfa68ba
krishna-sk12 <krishnayadav1798@gmail.com>
2022-07-09 20:59:12 +0530

    added files

---
+++ dolrtorupee.js
@@ -0,0 +1,11 @@
+        
+
+        function myfunctionup(myForm) {
+            var doller = myForm.doller.value
+            var rps = doller * 74
+            myForm.rupee.value = rps
+            
+        }
+
+
+    </script>
